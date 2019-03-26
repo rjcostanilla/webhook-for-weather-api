846daf6ac97a0c392ada4fe854174c3bd1fcd598
RJ <roentgen.costanilla@aboitiz.com>
2019-03-26 21:07:23 +0800

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -9,7 +9,7 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', method=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
@@ -49,6 +49,7 @@ def makeResponse(req):
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
+
     app.run(debug=True, port=port, host='0.0.0.0')
 
 
