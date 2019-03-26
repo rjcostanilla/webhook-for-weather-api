092118eb32aa4755f09faa6882c9b35c8b22e403
RJ <roentgen.costanilla@aboitiz.com>
2019-03-26 19:22:58 +0800

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -18,7 +18,7 @@ def webhook():
     
     res = json.dumps(res, indent=4)
     # print(res)
-    r = makeresponse(res)
+    r = makeResponse(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
